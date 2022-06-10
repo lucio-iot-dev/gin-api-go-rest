731cdd82b4ca31c955b70e26cd8dfeb9a9c87eed
Lúcio do Nascimento <luciomate95@gmail.com>
2022-06-09 23:35:53 -0300

    criando um endpoint para escrever uma mensagem na pagina utilizando qualquer nome digitado na url

--- routes/routes.go
+++ routes/routes.go
@@ -8,5 +8,9 @@ import (
 func HandleRequests() {
 	r := gin.Default()
 	r.GET("/alunos", controllers.ExibeTodosAlunos)
+	r.GET("/:nome", controllers.Saudacao)
+
+
+
 	r.Run() // porta padão é 8080 do gin, caso queira subir em outra porta é so passar entre aspas dentro do parenteses do Run o :5000 por exemplo
 }
