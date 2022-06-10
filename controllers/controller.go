20234b895e9490c8f8fb7c037aa6e7f37af595c9
Lúcio do Nascimento <luciomate95@gmail.com>
2022-06-09 23:52:43 -0300

    criando uma struct de alunos em uma pasta models

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,12 +1,12 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lucio-iot-dev/gin-api-go-rest/models"
+)
 
 func ExibeTodosAlunos(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"id":   "1",
-		"nome": "Gui Lima",
-	})
+	c.JSON(200, models.Alunos)
 }
 
 func Saudacao(c *gin.Context) {
---
+++ models/aluno.go
@@ -0,0 +1,11 @@
+package models
+
+type Aluno struct {
+	Nome string `json:"nome"`
+	CPF  string `json:"cpf"`
+	RG   string `json:"rg"`
+}
+
+var Alunos []Aluno
+
+
