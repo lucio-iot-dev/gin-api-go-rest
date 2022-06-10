bd8ff60e6550a9eef8d0dbe9cec32266024d3161
Lúcio do Nascimento <luciomate95@gmail.com>
2022-06-09 23:16:59 -0300

    modularizando o código

--- main.go
+++ main.go
@@ -1,18 +1,10 @@
 package main
 
-import(
-	"github.com/gin-gonic/gin"
+import (
+	"github.com/lucio-iot-dev/gin-api-go-rest/routes"
 )
 
-func ExibeTodosAlunos(c *gin.Context) {
-  c.JSON(200, gin.H{
-		"id":"1",
-		"nome":"Gui Lima",
-	})
-}
-
 func main() {
-	r := gin.Default()
-	r.GET("/alunos", ExibeTodosAlunos )
-	r.Run() // porta padão é 8080 do gin, caso queira subir em outra porta é so passar entre aspas dentro do parenteses do Run o :5000 por exemplo
+	routes.HandleRequests()
+
 }
---
+++ routes/routes.go
@@ -0,0 +1,12 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/lucio-iot-dev/gin-api-go-rest/controllers"
+)
+
+func HandleRequests() {
+	r := gin.Default()
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
+	r.Run() // porta padão é 8080 do gin, caso queira subir em outra porta é so passar entre aspas dentro do parenteses do Run o :5000 por exemplo
+}
