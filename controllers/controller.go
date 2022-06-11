583d3fb7115a1458df8f071dc2c3c0af0df9a7f0
Lúcio do Nascimento <luciomate95@gmail.com>
2022-06-10 21:15:04 -0300

    inplementação de um if para retornar uma mensagem de erro caso o id não existir no banco de dados quando fizer uma busca

--- controllers/controller.go
+++ controllers/controller.go
@@ -37,5 +37,11 @@ func BuscaAlunoPorID(c *gin.Context) {
   var aluno models.Aluno
 	id := c.Params.ByName("id")
 	database.DB.First(&aluno, id)
-	c.JSON(http.StatusOK, aluno)
-}
+
+  if aluno.ID == 0 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"Not found": "Aluno não encontrado",})
+			return
+		}
+			c.JSON(http.StatusOK, aluno)
+	}
