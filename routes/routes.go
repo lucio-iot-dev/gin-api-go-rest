1b31ea0d3c5f973701faab33c4c0f9d3ffff5bdf
Lúcio do Nascimento <luciomate95@gmail.com>
2022-06-14 21:20:50 -0300

    Melhoria da rota Put

--- routes/routes.go
+++ routes/routes.go
@@ -13,7 +13,7 @@ func HandleRequests() {
 	r.GET("/alunos/:id", controllers.BuscaAlunoPorID)
 	r.DELETE("/alunos/:id", controllers.DeletaAluno)
 	r.GET("/alunos/cpf/:cpf", controllers.BuscaAlunoPorCPF)
-	r.PUT("/alunos", controllers.AtualizaAluno)
+	r.PUT("/alunos/:id", controllers.AtualizaAluno)
 
 	r.Run() // porta padão é 8080 do gin, caso queira subir em outra porta é so passar entre aspas dentro do parenteses do Run o :5000 por exemplo
 }
